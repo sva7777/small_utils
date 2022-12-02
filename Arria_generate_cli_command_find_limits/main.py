01d77cb749184d9128fee7dad05deddad5fa59f9
vasily <sva7777@gmail.com>
2022-12-02 17:08:18 +0300

    first real implementation what generates cli command

--- Arria_generate_cli_command_find_limits/main.py
+++ Arria_generate_cli_command_find_limits/main.py
@@ -1,7 +1,11 @@
+import ipaddress
 
-def add_multi_ip(start, count):
-    print("test")
+def add_multi_ip(network, count):
+    array= [str(ip) for ip in ipaddress.IPv4Network(network)]
+
+    for i in range(1,count+1):
+        print('configure interface 1 rule add ip={} to 3 type=direct status=active name="test"'.format(array[i]))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    add_multi_ip("192.168.1.1",100)
+    add_multi_ip("192.169.0.0/16",253)
